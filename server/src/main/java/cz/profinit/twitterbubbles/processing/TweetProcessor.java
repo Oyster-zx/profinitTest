71001109fe768276e2d838e0dabe109e9c0e559c
Igor Kuzevanov <igor.kuzevanov@seznam.cz>
2018-04-12 18:26:14 +0200

    first task

--- server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
+++ server/src/main/java/cz/profinit/twitterbubbles/processing/TweetProcessor.java
@@ -1,10 +1,14 @@
 package cz.profinit.twitterbubbles.processing;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
 import cz.profinit.twitterbubbles.model.TweetStats;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Stream;
 
 @Component
 public class TweetProcessor {
@@ -19,9 +23,23 @@ public class TweetProcessor {
         // TODO Rozdělení textu do slov a spočítání počtu jejich výskytů.
         // TODO Implementace je hotová, pokud uspěje unit test TweeProcessorTest.
 
-        TweetStats tweetStats = TweetStats.EMPTY;
-        String[] res = text.toLowerCase().split(" ");
+        Map<String, Integer> resultMap = new HashMap<>();
+        Arrays.stream(text.toLowerCase().split(" ")).forEach(s->{
+            List<Character> chrs = new ArrayList<>();
+            for(char ch : s.toCharArray()){
+                chrs.add(ch);
+            }
+            String filteredS = chrs.stream()
+                    .filter(Character::isLetter)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+            Integer count = resultMap.get(filteredS);
+            if (count == null){
+                resultMap.put(filteredS, 1);
+            }else{
+                resultMap.put(filteredS, ++count);
+            }
+        });
 
-        return TweetStats.DUMMY;
+        return new TweetStats(resultMap);
     }
 }
